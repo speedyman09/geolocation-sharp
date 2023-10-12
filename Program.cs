6ab7fdb11d1d752329272d2ac44be2d777873bea
stephen <stephen.jr@selyuk.com>
2023-10-12 19:32:41 +0300

    Initial;

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace geolocation_sharp;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            switch(arg)
+            {
+                case not null when arg.Contains("-f"):
+                    Console.WriteLine("ARG had a -f flag");
+                    break;
+                case not null when arg.Contains("-a"):
+                    Console.WriteLine("ARG had a -a flag");
+                    break;
+            }
+        }
+    }
+}
+
